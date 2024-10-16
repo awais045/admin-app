fa07c5d95f4f712b46d88fdcf7fc0c721797b875
CORP\muhammadaw.zulifqar <muhammadawais.zulifqar@ibex.co>
2024-10-16 23:26:13 +0500

    chanhe

--- app/Forms/Admin/SubDomainForm.php
+++ app/Forms/Admin/SubDomainForm.php
@@ -11,8 +11,8 @@ class SubDomainForm extends Form
 
     public function buildForm()
     {
-        $roles = Role::all();
-        $userHasRoles = [];
+        // $roles = Role::all();
+        // $userHasRoles = [];
 
         // if ($this->model) {
         //     $userHasRoles = array_column(json_decode($this->model->roles, true), 'name');
@@ -20,12 +20,12 @@ class SubDomainForm extends Form
 
         $this->add('name', 'text', [
             'label' => __('Client Name'),
-            'value' => 'name',
+            'value' => '',
         ]);
 
         $this->add('domain_name', 'text', [
             'label' => __('Domain Name'),
-            'value' => 'domain_name',
+            'value' => '',
         ]);
 
 
@@ -36,24 +36,24 @@ class SubDomainForm extends Form
                 'local' => 'Internal',
                 'remote' => 'External',
             ],
-            'value' => 'subdomain_type',
+            'value' => '',
         ]);
         $this->add('subdomain', 'text', [
             'label' => __('Domain URL'),
-            'value' => 'subdomain',
+            'value' => '',
             'pattern' => '^[a-zA-Z0-9]+$',
             'title' => "Domain name must be a single word without spaces or special characters.",
         ]);
 
         $this->add('domain_dns1', 'text', [
             'label' => __('Domain DNS1'),
-            'value' => 'domain_dns1',
+            'value' => '',
             'attributes' => ['id' => 'domain_dns1'], // For JS handling
         ]);
 
         $this->add('domain_dns2', 'text', [
             'label' => __('Domain DNS2'),
-            'value' => 'domain_dns2',
+            'value' => '',
             'attributes' => ['id' => 'domain_dns2'], // For JS handling
         ]);
 
@@ -61,7 +61,7 @@ class SubDomainForm extends Form
 
         $this->add('db_name', 'text', [
             'label' => __('Database Name'),
-            'value' => 'db_name',
+            'value' => '',
         ]);
 
         $this->add('db_type', 'select', [
@@ -76,19 +76,19 @@ class SubDomainForm extends Form
 
         $this->add('hostname', 'text', [
             'label' => __('Host Name'),
-            'value' => 'hostname',
+            // 'value' => 'hostname',
             'attributes' => ['id' => 'hostname'], // For JS handling
         ]);
 
         $this->add('db_user', 'text', [
             'label' => __('Database User'),
-            'value' => 'db_user',
+            // 'value' => 'db_user',
             'attributes' => ['id' => 'db_user'], // For JS handling
         ]);
 
         $this->add('db_password', 'text', [
             'label' => __('Database Password'),
-            'value' => 'db_password',
+            // 'value' => 'db_password',
             'attributes' => ['id' => 'db_password'], // For JS handling
         ]);
 
